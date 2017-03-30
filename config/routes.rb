86e85b9b80e2e47ee564bf09641b2e77414bd24d
SegueX <free1989xj@hotmail.com>
2017-03-30 17:32:30 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,11 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
+    resources :posts
   end
-  
+
+  resources :posts
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
